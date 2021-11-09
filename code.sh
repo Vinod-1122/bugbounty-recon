2ad14d58c530b0a02390b8f1c3df57b006498e69
Vinod Sangepu <62217877+Vinod-1122@users.noreply.github.com>
2021-11-09 14:53:24 +0530

    Update code.sh

--- code.sh
+++ code.sh
@@ -12,13 +12,13 @@ cd $target
 python3 /home/offensivehunter/Sublist3r/sublist3r.py -d $target > subdomains.txt
 
 # Now find another subdomains from assetfinder 
-assetfinder -subs-only $target >> subdomains.txt
+assetfinder -subs-only $target >> subdomains1.txt
 
 # Now we do sorting to throw out duplicate subdomains
-cat subdomains.txt | sort | uniq >> subdomains1.txt
+cat subdomains.txt subdomains.txt | sort | uniq >> subdomains2.txt
 
 # Now we have to find live subdomains
-cat subdomains1.txt | httprobe > alive.txt 
+cat subdomains2.txt | httprobe > alive.txt 
 
 # Now we have to find Javascript files to scroll 
 cat alive.txt | subjs > jsfiles.txt
@@ -29,5 +29,5 @@ python3 /home/offensivehunter/dirsearch/dirsearch.py -u alive.txt -w /home/offen
 # This is the last thing to do subdomain takeover
 while read -r line
 do
-    subzy --targets subs.txt --concurrency 20 --verify_ssl
+    subzy --targets subdomains2.txt --concurrency 20 --verify_ssl
 done
