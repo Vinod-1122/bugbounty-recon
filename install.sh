659039b6f0bcbc9839d7ed5fa6dc3eabd213e0d4
Vinod Sangepu <62217877+Vinod-1122@users.noreply.github.com>
2021-11-09 13:19:04 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/Vinod-1122/bugbounty-recon.git
+
+chmod u+x code.sh     
+
+./code.sh -d ( enter your target here ) 
+
+like this 
+
+./code.sh -d google.com 
+
+The process will automatically works 
